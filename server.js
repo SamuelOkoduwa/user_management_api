ba1e734dd56ba2c5ac2c5533b24dfbd9c3a2aac0
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2025-07-02 15:35:00 +0100

    application setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require('./src/app');
+const http = require('http');
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+	console.log('Server is running on port 3000');
+})
+
+module.exports = server;
