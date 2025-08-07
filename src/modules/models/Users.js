afab27ebd5d8a19457c9f0bf0f36b2ccae30058c
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2025-08-07 10:32:59 +0100

    added the user model

---
+++ src/modules/models/Users.js
@@ -0,0 +1,30 @@
+// create a new file src/modules/models/Users.js
+const { db}= require('../../config/database');
+const { DataTypes } = require('sequelize');
+
+const User = db.define('User', {
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false
+	},
+	email: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		unique: true
+	},
+	password: {
+		type: DataTypes.STRING,
+		allowNull: false
+	}
+}, {
+    // Additional options
+    tableName: 'users', 
+	timestamps: true
+});
+
+module.exports = User;
